98cf23ea3b50781c61ff75bfe9a3c71537db95d2
Араканцев Константин Геннадьевич <k.arakantsev@bftcom.com>
2022-06-13 21:43:17 +0300

    add user greeting

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
 
+import { greet } from "../src/cli.js"
+
 console.log('Welcome to the Brain Games!');
+
+greet();
---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readLineSync from "readline-sync"
+
+function greet() {
+    const name = readLineSync.question("May I have your name? ");
+    console.log(`Hello, ${name}!`);
+}
+
+export { greet };
