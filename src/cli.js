8a0830a2205a3ddea7d24841c6dec9c07e6989f5
Араканцев Константин Геннадьевич <k.arakantsev@bftcom.com>
2022-06-16 16:32:29 +0300

    add brain even game

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,18 @@
 import readLineSync from 'readline-sync';
 
-function greet() {
-  const name = readLineSync.question('May I have your name? ');
+export function getUserInput(prompt) {
+  return readLineSync.question(prompt);
+}
+
+export function greet() {
+  const name = getUserInput('May I have your name? ');
   console.log(`Hello, ${name}!`);
 }
 
-export { greet };
+export function getRandomNumber() {
+  return Math.round(Math.random() * 100);
+}
+
+export function isNumberEven(num) {
+  return num % 2 === 0;
+}
